63cc301ffcdf579309c035a0f444a472c961a3b6
basnetrajpradip <basnetrajpradip@gmail.com>
2024-03-31 19:36:13 +0545

    Implemented Doubly linked list in C

--- linked_list/doubly_linked_list.c
+++ linked_list/doubly_linked_list.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structure for doubly linked list node.
+struct ListNode {
+  int data;
+  struct ListNode *next;
+  struct ListNode *prev;
+};
+
 // Main function
 int main() {}
