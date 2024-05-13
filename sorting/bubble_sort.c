8d2c13009658ab94098620cd0a0133f9d60f9cdc
basnetrajpradip <basnetrajpradip@gmail.com>
2024-05-13 20:14:43 +0545

    Implemented bubble sort in c

---
+++ sorting/bubble_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*  It is called bubble sort as just like bubbles in water, each element of the
+ array bubbles towards it's correct position(towards the end) in each
+ iteration. */
+void bubbleSort(int arr[], int size) {
+  int temp;
+  // loop to access elements of array
+  for (int i = 0; i < size - 1; i++) {
+    // loop to compare array elements
+    for (int j = 0; j < size - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d\t", arr[i]);
+  }
+  printf("\n");
+}
+
+int main(void) {
+  int arr[] = {4, 1, 2, 9, 3, 8, 7, 14, 6};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  printf("Input array is: \n");
+  printArray(arr, size);
+
+  bubbleSort(arr, size);
+  printf("Sorted Array is: \n");
+  printArray(arr, size);
+}
